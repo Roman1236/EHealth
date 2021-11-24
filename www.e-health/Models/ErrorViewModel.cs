e20a08fbd045f2990bc75f335bd34edd3da09ac2
Andrii_Hrytsachenko <andrii.hrytsachenko@evinent.com>
2021-11-24 21:24:30 +0300

    Initial project

---
+++ www.e-health/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace www.e_health.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
